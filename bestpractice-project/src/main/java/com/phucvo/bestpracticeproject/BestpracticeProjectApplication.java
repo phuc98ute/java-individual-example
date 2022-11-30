02cdeeadca620acc898bd27931eba066edfd7b9f
phuc98ute <vophuc3@gmail.com>
2022-11-30 11:30:01 +0700

    init project commit

---
+++ bestpractice-project/src/main/java/com/phucvo/bestpracticeproject/BestpracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.phucvo.bestpracticeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestpracticeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestpracticeProjectApplication.class, args);
+    }
+
+}
---
+++ bestpractice-project/src/test/java/com/phucvo/bestpracticeproject/BestpracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucvo.bestpracticeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestpracticeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
